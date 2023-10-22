15f2ee0a2f9e84503a9a557fb7bac1a3b08334d3
tamanna986 <tamanna.986jahan@gmail.com>
2023-10-22 19:51:27 +0600

    product details  fetched from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -49,7 +49,35 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     });
+  
+    app.get('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollection.findOne(query);
+        res.send(result);
+    });
+ 
     
+    
+    
+
+    // app.get('/products/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = {_id: new ObjectId(id)};
+    //     const cursor = productCollection.findOne(query);
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+    // });
+
+    // app.get('/products/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) };
+    //     const result = await productCollection.findOne(query);
+    //     res.send(result);
+    // });
+    
+
+  
 
     // for adding products
 
